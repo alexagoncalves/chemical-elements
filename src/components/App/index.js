f0b3e25a67e08f6d7dfa0c29d46f1a8a2bc7a84c
alexagoncalves <alexandragoncalves.web@gmail.com>
2022-08-22 19:44:51 +0200

    carousel component integration done

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,11 @@
 // == Import
-import reactLogo from './react-logo.svg';
 import './styles.css';
+import Carousel from '../Carousel';
 
 // == Composant
 const App = () => (
   <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
+    <Carousel/>
   </div>
 );
 
---
+++ src/components/Carousel/index.js
@@ -0,0 +1,18 @@
+import './carousel.scss';
+
+const Carousel = () => {
+
+    return (
+        <div className="carousel">
+            <ul className="carousel__list">
+                <li className="carousel__item alkalimetal"  data-pos="-2"></li>
+                <li className="carousel__item alkalineearthmetal" data-pos="-1"></li>
+                <li className="carousel__item actinoid" data-pos="0" data-before='Hs' id="1"><span className="carousel__item-number">108</span></li>
+                <li className="carousel__item actinoid" data-pos="1"></li>
+                <li className="carousel__item actinoid" data-pos="2"></li>
+            </ul>
+        </div>
+    )
+};
+
+export default Carousel;
